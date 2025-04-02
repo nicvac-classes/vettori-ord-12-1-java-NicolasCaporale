387d7ce61b3ad8129b15e17d2ded30c855c9a675
NicolasCaporale <nicolas.caporale@marconibari.edu.it>
2025-04-02 09:39:22 +0200

    Update Esercizio.java

--- Esercizio.java
+++ Esercizio.java
@@ -2,25 +2,130 @@
 
 //Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
+import java.util.Random;
 
 // Classe principale, con metodo main
 class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+    
+    private static Scanner in = new Scanner(System.in);
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+    public static void main(String args[]){
+    
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        int N, contConf;
+        
+        System.out.print("Inserire la grandezza del vettore: ");
+        N = in.nextInt();
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        int[] R = new int[N];
+
+        RiempiVettore(R,N);
+        contConf = BubbleSort0(R,N);
+        System.out.println("Vettore Ordinato: ");
+        VisualizzaVettore(R,N);
+        System.out.println("Numero di confronti: " + contConf);
+
+        RiempiVettore(R,N);
+        contConf = BubbleSort1(R,N);
+        System.out.println("Vettore Ordinato: ");
+        VisualizzaVettore(R,N);
+        System.out.println("Numero di confronti: " + contConf);
+
+        RiempiVettore(R,N);
+        contConf = BubbleSort2(R,N);
+        System.out.println("Vettore Ordinato: ");
+        VisualizzaVettore(R,N);
+        System.out.println("Numero di confronti: " + contConf);
+    }
+
+    public static void RiempiVettore(int[] V, int N){
+        for(int i = 0; i < N; ++i){
+            V[i] = (int) (Math.random() * (N * 10));
+        }
+    }
+
+    public static void VisualizzaVettore(int[] V, int N){
+        for(int i = 0; i < N; ++i){
+            System.out.println(i + "Â° numero: " + V[i]);
+        }
+    }
+
+    public static int BubbleSort0(int[] V, int N){
+        int conf;
+        int i, j, t;
+
+        conf = 0;
+        i = 0;
+
+        while(i < N){
+            j = 0;
+            
+            while(j < N - 1){
+                conf++;
+                if(V[j] > V[j + 1]){
+                    t = V[j];
+                    V[j] = V[j+i];
+                    V[j+1] = t;
+                }
+                j++;
+            }
+            i++;
+        }
+
+        return conf;
+    }
+    
+    public static int BubbleSort1(int[] V, int N){
+        int conf;
+        int i, j, t;
+
+        conf = 0;
+        i = 0;
+
+        while(i < N){
+            j = 0;
+            
+            while(j < (N - 1)){
+                conf++;
+                if(V[j] > V[j + 1]){
+                    t = V[j];
+                    V[j] = V[j+i];
+                    V[j+1] = t;
+                }
+                j++;
+            }
+            i++;
+        }
+        
+        return conf;
+    }
+
+    public static int BubbleSort2(int[] V, int N){
+        int conf;
+        int i, j, t;
+        boolean confronto = true;
+
+        conf = 0;
+        i = 0;
+
+        while(i < N && confronto){
+
+            confronto = false;
+            j = 0;
+            
+            while(j < (N - 1) - i){
+                conf++;
+                if(V[j] > V[j + 1]){
+                    t = V[j];
+                    V[j] = V[j+i];
+                    V[j+1] = t;
+                    confronto = true;
+                }
+                j++;
+            }
+            i++;
+        }
+        
+        return conf;
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
